f23e7b74c509e8d76c0cbb7f845e90b93f861543
Paul Wayper <paulway@mabula.net>
2021-09-20 20:27:29 +1000

    First attempt at an existing event key validator
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- project/dictgame/forms.py
+++ project/dictgame/forms.py
@@ -3,7 +3,13 @@ from django import forms
 from dictgame.models import Player, Event, Question
 
 
+def existing_event(key):
+    return Event.objects.filter(key=key).exists()
+
+
 class EventForm(forms.ModelForm):
+    key = forms.SlugField(validators=[existing_event])
+
     class Meta:
         model = Event
         fields = ('key', )
