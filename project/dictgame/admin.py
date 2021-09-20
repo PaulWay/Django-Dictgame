4b95f1a09a8cb03bd06f874518d6abf63afe4a4d
Paul Wayper <paulway@mabula.net>
2021-09-20 20:27:03 +1000

    Adding basic Django model administration
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- project/dictgame/admin.py
+++ project/dictgame/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from dictgame.models import Event, Player, Question
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
