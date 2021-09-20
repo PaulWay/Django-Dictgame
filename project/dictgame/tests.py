77d891e0c8a71cfade61a7d76f01fcb547a6e16a
Paul Wayper <paulway@mabula.net>
2021-09-20 20:26:36 +1000

    Adding some test data and testing joining an event
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- project/dictgame/tests.py
+++ project/dictgame/tests.py
@@ -9,9 +9,22 @@ class constants():
 
 
 class EntryTestCase(TestCase):
+    fixtures = ['basic_test_data',]
 
     def test_basic_entry_page(self):
         response = self.client.get(reverse('entry'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], constants.html_content_type)
         self.assertIn('<html>', response.content.decode())
+
+    def test_entry_form(self):
+        # Get an error and the form again
+        response = self.client.post(
+            reverse('entry'),
+            data={'key': 'Nostalgic burblings'},
+        )
+        self.assertEqual(response.status_code, 200)  # Not a 400
+        self.assertEqual(response.headers['Content-Type'], constants.html_content_type)
+        content = response.content.decode()
+        self.assertIn('<html>', content)  # And it starts the usual way,
+        self.assertIn('<ul class="errorlist">', content)  # but it has an error message
