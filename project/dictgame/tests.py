59e981aae06b393db90d0e42d42ed718743ace60
Paul Wayper <paulway@redhat.com>
2021-09-20 16:43:19 +1000

    Added very basic tests to start
    Signed-off-by: Paul Wayper <paulway@redhat.com>

--- project/dictgame/tests.py
+++ project/dictgame/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class constants():
+    html_content_type = 'text/html; charset=utf-8'
+
+
+class EntryTestCase(TestCase):
+
+    def test_basic_entry_page(self):
+        response = self.client.get(reverse('entry'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['Content-Type'], constants.html_content_type)
+        self.assertIn('<html>', response.content.decode())
