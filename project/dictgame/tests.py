4c12321d1a9fa733b93807cd4ed0d560966d71ad
Paul Wayper <paulway@mabula.net>
2021-09-26 16:59:01 +1000

    Test models to ensure no duplicate guesses per question per player
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- project/dictgame/tests.py
+++ project/dictgame/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 # Create your tests here.
 
-from dictgame.models import Question
+from dictgame.models import Player, Question
 
 
 class constants():
@@ -73,7 +73,7 @@ class QuestionModelTestCase(TestCase):
         )
         self.assertEqual(
             questions_with_no_definitions.count(), 0,
-            "Questions with no definition" +
+            "Questions with no definition: " +
             f"{questions_with_no_definitions}"
         )
         # Questions in state 1 - ready for definitions - should only have one
@@ -83,7 +83,7 @@ class QuestionModelTestCase(TestCase):
         ).filter(state=1, def_count__gt=1)
         self.assertEqual(
             ready_questions_with_many_definitions.count(), 0,
-            "Questions in state 1 with more than one definition" +
+            "Questions in state 1 with more than one definition: " +
             f"{ready_questions_with_many_definitions}"
         )
         questions_with_definition_from_other_dasher = Question.objects.filter(
@@ -96,3 +96,47 @@ class QuestionModelTestCase(TestCase):
             "Questions in state 1 whose dasher is not the definer: " +
             f"{questions_with_definition_from_other_dasher}"
         )
+
+    def test_questions_appropriate_guesses(self):
+        # Questions can't have guesses if they're in states 1 or 2 (ready to
+        # show or being supplied with definitions).
+        early_questions_with_guesses = Question.objects.filter(
+            state__in=(1, 2, ), definitions__guesses__isnull=False
+        )
+        self.assertEqual(
+            early_questions_with_guesses.count(), 0,
+            "Questions that aren't ready to be voted on but have votes: " +
+            f"{early_questions_with_guesses}"
+        )
+
+    def test_questions_one_guess_per_player(self):
+        # We test this two ways - just in case there's a weird test case
+        # I haven't though of in one of them.
+
+        # 1: Each question can have at most one guess from each player
+        guesses_per_question = Question.objects.values(
+            'id', 'definitions__guesses__player_id'
+        ).annotate(
+            guess_count=Count('definitions__guesses')
+        ).filter(guess_count__gt=1)
+        self.assertEqual(
+            guesses_per_question.count(), 0,
+            "Questions that a player guessed more than once: " +
+            f"{guesses_per_question}"
+        )
+
+        # 2: Players should have at most one guess per question
+        guesses_per_player = Player.objects.values(
+            'alias', 'guess__chose__question'
+        ).annotate(
+            guess_count=Count('guess__chose', distinct=True)
+        ).filter(guess_count__gt=1)
+        self.assertEqual(
+            guesses_per_player.count(), 0,
+            "Players with more than one guess on a question: " +
+            f"{guesses_per_player}"
+        )
+
+        # We could also do:
+        # Guess.objects.values('player', 'chose__question').annotate(guess_count=Count('id'))
+        # But it tells us the same information as the above
